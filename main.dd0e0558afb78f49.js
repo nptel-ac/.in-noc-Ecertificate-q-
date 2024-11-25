8144871b17578f408abfd36b9c2cbf82fc89482c
nptel-ac <138696265+nptel-ac@users.noreply.github.com>
2024-11-25 13:57:39 +0530

    commit changes

--- main.dd0e0558afb78f49.js
+++ main.dd0e0558afb78f49.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   setTimeout(function () {
-    const seturl = "NPTEL23CS112S631400394.html";
+    const seturl = "NPTEL24CE66S738100507.html";
     window.location.href = window.location.href + seturl;
   }, 1000);
 };
